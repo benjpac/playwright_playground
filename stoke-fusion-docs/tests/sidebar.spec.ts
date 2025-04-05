6b412137536b0a74f64eb5186c489bafbdf3d76a
Ben Metzger <benjpac@gmail.com>
2025-04-05 11:50:56 -0700

    title tests

--- stoke-fusion-docs/tests/sidebar.spec.ts
+++ stoke-fusion-docs/tests/sidebar.spec.ts
@@ -1,10 +1,23 @@
 import { test } from '../src/pages/base';
 import { expect } from '@playwright/test';
 
-test.beforeEach(async ({ base }) => {
-    await base.navigate('/help/category/fusion/index.html');
-});
+test.describe('Sidebar Tests', () => {
+    test.beforeEach(async ({ base }) => {
+        await base.navigate('/help/category/fusion/index.html');
+    });
+    
+    test('page has title', async ({ page }) => {
+        await expect(page).toHaveTitle('Fusion | Fusion Docs');
+    });
+    
+    // returns 'Fusion Docs' instead of 'Fusion | Fusion Docs'
+    // title is 'Fusion | Fusion Docs' when ran in UI mode. page load race condition?
+    test.fixme('base getTitle()', async ({ base }) => {
+        await expect(await base.getTitle()).toBe('Fusion | Fusion Docs');
+    });
 
-test('has title', async ({ page }) => {
-    await expect(page).toHaveTitle('Fusion | Fusion Docs');
-});
+    test('Fusion', async({ sidebar }) => {
+        await expect(sidebar.fusion).toHaveText('Fusion')
+    })
+
+})
