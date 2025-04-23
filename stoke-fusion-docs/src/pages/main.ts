ad8669a36189aace3ae34463ce3d3de2ed42a773
Ben Metzger <benjpac@gmail.com>
2025-04-22 17:42:32 -0700

    WIP aria snapshot test. Not a test I would use for this, but why doesn't it work?

--- stoke-fusion-docs/src/pages/main.ts
+++ stoke-fusion-docs/src/pages/main.ts
@@ -4,11 +4,13 @@ export class Main {
   readonly page: Page;
   readonly next: Locator;
   readonly previous: Locator;
+  readonly content: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.next = page.getByRole('link', { name: 'Next' });
     this.previous = page.getByRole('link', { name: 'Previous' });
+    this.content = page.getByRole('main').locator('div').first();
   }
 
   async getH1(title: string): Promise<Locator> {
--- stoke-fusion-docs/tests/main.spec.ts
+++ stoke-fusion-docs/tests/main.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '../src/fixtures/navigation'
+import fs from 'fs'
 
 test.describe('Main Container', () => {
     test.beforeAll(({ flattenedLinks }) => {
@@ -6,20 +7,35 @@ test.describe('Main Container', () => {
         if (invalid.length) throw new Error(`Invalid URLs: ${JSON.stringify(invalid)}`)
     })
 
-    test('all pages', async ({ page, main, baseURL, flattenedLinks }) => {
+    test.only('all pages', async ({ page, main, baseURL, flattenedLinks }) => {
+        test.setTimeout(60000); // Set timeout to 60 seconds for this test only
+
         for (const link of flattenedLinks) {
             console.log(link);
 
-            await test.step(`go to: ${link.url}`, async () => {
+            await test.step(`go to "${link.url}"`, async () => {
                 await page.goto(`/help${link.url}/index.html`);
+
                 // ensure we're at the correct URL
                 await expect(page).toHaveURL(`${baseURL}/help${link.url}/index.html`);
             })
 
-            await test.step(`h1 is: ${link.text}`, async () => {
+
+            await test.step(`h1 is "${link.text}"`, async () => {
                 const h1 = await main.getH1(link.text)
+
                 // compare expected h1 text from navigation.json with the current h1 text on the page
                 await expect(h1, `should it be "${await page.locator('h1').first().textContent()}"?`).toHaveText(link.text);
+            })
+
+            await test.step('aria snapshot', async () => {
+                const ariaSnapshot = await main.content.ariaSnapshot()
+
+                const ariaSnapshotPath = `../src/data/aria-snapshots/main/${link.url}.json`
+
+                fs.writeFileSync(ariaSnapshotPath, JSON.stringify(ariaSnapshot, null, 2))
+
+                // await expect(main.content).toMatchAriaSnapshot(ariaSnapshotPath)
             })
         }
     })
